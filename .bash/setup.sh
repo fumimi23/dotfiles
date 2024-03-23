af0dc553ad8c0616e9344b7ce788a24f7dd2e8aa
fumimi23 <122011985+fumimi23@users.noreply.github.com>
2024-03-23 23:22:36 +0900

    feat: update setup script

--- .bash/setup.sh
+++ .bash/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# apt udate
+sudo apt update
+sudo apt upgrade
+
 # nodenv
 git clone https://github.com/nodenv/nodenv.git ~/.nodenv
 exec $SHELL -l
@@ -11,4 +15,4 @@ exec $SHELL -l
 git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 exec $SHELL -l
-
+sudo apt install build-essential libbz2-dev libdb-dev libreadline-dev libffi-dev libgdbm-dev liblzma-dev libncursesw5-dev libsqlite3-dev libssl-dev zlib1g-dev uuid-dev tk-dev
